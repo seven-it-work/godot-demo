f5122aea59040084e3ace3475f0ebae415284396
seven <sevenyjl@gmail.com>
2025-03-07 20:05:32 +0800

    补充相关插件

---
+++ 修仙1.0/settings/dist/settings.gd
@@ -0,0 +1,8 @@
+# warnings-disable
+extends Node
+# 这个脚本你需要挂到游戏的Autoload才能全局读表
+
+var 地点 = load('res://settings/dist/基础数据/地点.gd').new()
+var 怪物 = load('res://settings/dist/基础数据/怪物.gd').new()
+var 技能 = load('res://settings/dist/基础数据/技能.gd').new()
+var 路径 = load('res://settings/dist/基础数据/路径.gd').new()
---
+++ 修仙1.0/settings/dist/基础数据/技能.gd
@@ -0,0 +1,18 @@
+# warnings-disable
+extends RefCounted
+
+@warning_ignore("unused_variable")
+var True = true
+@warning_ignore("unused_variable")
+var False = false
+@warning_ignore("unused_variable")
+var None = null
+
+
+var data = \
+{
+1:{ "id":1,  "名称":'普通攻击',  "FileName":'BaseAttack', },
+2:{ "id":2,  "名称":'普通防御',  "FileName":'BaseDefense', },
+
+}
+
